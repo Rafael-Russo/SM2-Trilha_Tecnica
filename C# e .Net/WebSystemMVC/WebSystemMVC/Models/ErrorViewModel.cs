e4a606a38170890d3f1e7a60e97780d7ae3d8d46
Rafael-Soares <11800585@aluno.cotemig.com.br>
2023-09-04 10:54:16 -0300

    termino do curso de C# (remocao do projeto 'WebSystemMVC')

--- C# e .Net/WebSystemMVC/WebSystemMVC/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace WebSystemMVC.Models {
-    public class ErrorViewModel {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
