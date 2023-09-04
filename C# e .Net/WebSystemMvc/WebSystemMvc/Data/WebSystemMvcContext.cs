a8c249f5bb7a7ee35b33ed95e17b4c2cf7af325e
Rafael-Soares <11800585@aluno.cotemig.com.br>
2023-09-04 17:47:18 -0300

    Reorganizacao e adicionando novamente o projeto .NET WebSystemMvc

---
+++ C# e .Net/WebSystemMvc/WebSystemMvc/Data/WebSystemMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebSystemMvc.Models;
+
+namespace WebSystemMvc.Data
+{
+    public class WebSystemMvcContext : DbContext
+    {
+        public WebSystemMvcContext (DbContextOptions<WebSystemMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebSystemMvc.Models.Department> Department { get; set; }
+    }
+}
---
+++ C# e .Net/WebSystemMvc/WebSystemMvc/Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace WebSystemMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
